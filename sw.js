bee8d9ab08dca286491616db89e7d330e999419e
Smitch86 <si_mitchell86@hotmail.co.uk>
2026-03-24 14:17:33 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'liftlog-v4';
+const ASSETS = [
+  '/torsolimbstracker/',
+  '/torsolimbstracker/index.html',
+  '/torsolimbstracker/manifest.json',
+  '/torsolimbstracker/icon-192.png',
+  '/torsolimbstracker/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800&family=Barlow:wght@300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        if (!response || response.status !== 200 || response.type === 'opaque') return response;
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      }).catch(() => caches.match('/torsolimbstracker/index.html'));
+    })
+  );
+});
